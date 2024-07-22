84017eed5c16cad31bf0db73c398d0cea9cdde40
RosaEvasari <erlinarosae@gmail.com>
2024-07-22 22:54:30 +0200

    commit_Rosa

---
+++ functions_Rosa.py
@@ -0,0 +1,48 @@
+# Read csv file
+
+def read_txt(file_path):
+    
+    file = pd.read_csv(file_path, delimiter=',') 
+    df = pd.DataFrame(file)
+    df = df.copy()
+
+    return df
+
+
+# Data exploration
+
+def data_exploration(df):
+
+    # check number of rows and columns
+    shape = df.shape
+    print("Number of rows:", shape[0])
+    print("Number of columns:", shape[1])
+
+    # check duplicates
+    check_duplicates = df.duplicated().sum()
+    print("Number of duplicates:", check_duplicates)
+
+    # Create a summary DataFrame
+    summary_df = pd.DataFrame({
+                        'Column': df.columns,
+                        'Data Type': df.dtypes,
+                        'Non-Null Count': df.notnull().sum(),
+                        'Missing Values': df.isnull().sum(),
+                        'Unique Values': df.nunique()
+                })
+
+    # Reset index to make 'Column' a regular column
+    summary_df.reset_index(drop=True, inplace=True)
+
+    # Display the summary DataFrame
+    summary_df
+    
+    # check numerical columns
+    numerical_columns = df.select_dtypes("number").columns
+    print("\nNumerical Columns:", numerical_columns)
+
+    # check categorical columns
+    categorical_columns = df.select_dtypes("object").columns
+    print("\nCategorical Columns:", categorical_columns)
+
+    return summary_df
